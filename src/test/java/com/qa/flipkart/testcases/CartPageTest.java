ec184e054fb32fdb293665fc9a606e5b311ba01e
monika-sys <61821528+monika-sys@users.noreply.github.com>
2020-07-27 14:40:24 +0530

    Update CartPageTest.java

--- src/test/java/com/qa/flipkart/testcases/CartPageTest.java
+++ src/test/java/com/qa/flipkart/testcases/CartPageTest.java
@@ -53,8 +53,4 @@ public class CartPageTest extends BaseClass {
 	public void tearDown() {
 		driver.quit();
 	}
-import com.qa.flipkart.base.BaseClass;
-
-public class CartPageTest extends BaseClass {
-
 }
